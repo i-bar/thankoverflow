ed458bf0543c7b988c89c9f01e0c8b6cd66dac8d
Ioana Barcan <ioana.barcan@thoughtworks.com>
2019-06-13 18:17:15 +0800

    Fixed build relative path.

--- server/server.js
+++ server/server.js
@@ -44,7 +44,7 @@ app.use(express.static(path.join(__dirname, "build")));
 // Order matters. Put all API endpoints above app.get("*")
 // Handles any requests that don't match the ones above
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/build/index.html"));
+  res.sendFile(path.join(__dirname + "./../build/index.html"));
 });
 
 const port = process.env.PORT || 5000;
