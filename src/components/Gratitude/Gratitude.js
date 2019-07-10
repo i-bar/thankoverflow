6ca450e8d48b40e72109534e45a4d9a6ac2b7826
i-bar <ioana.barcan@gmail.com>
2019-07-10 23:27:32 +0800

    Added border around readonly gratitude.

--- src/components/Gratitude/Gratitude.js
+++ src/components/Gratitude/Gratitude.js
@@ -1,7 +1,15 @@
 import React from "react";
+import "./Gratitude.css";
 
 const Gratitude = props => {
-  return <div className="gratitude">{props.message}</div>;
+  if (props.message)
+    return (
+      <div className="gratitude-wrapper">
+        <label>And somebody else is grateful for...</label>
+        <div className="gratitude">{props.message}</div>
+      </div>
+    );
+  else return "";
 };
 
 export default Gratitude;
